f371e43236ca382611ac42b59613f74936f4e053
zirahigarodrigue <rodrigueishara@gmail.com>
2023-05-10 13:49:29 +0300

    heroku deployment conf

--- sys_backend/settings.py
+++ sys_backend/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,10 +73,21 @@ WSGI_APPLICATION = 'sys_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd34cl19o9vjp2a',
+        'USER': 'garwzmeojvtbeh',
+        'PASSWORD': 'd014b381a7be061e849f335f054d5d9be7912b1a79176e5a99def558098b9b40',
+        'HOST': 'ec2-107-21-67-46.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -115,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
